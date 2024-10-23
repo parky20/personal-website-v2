7e097ebe9693039057682e913a8a5f10adc33438
park.yun <park.yun@northeastern.edu>
2024-10-23 17:28:15 -0400

    update

--- src/app/aboutMe/page.tsx
+++ src/app/aboutMe/page.tsx
@@ -6,7 +6,6 @@ export default function AboutMe() {
          <span className="text-lg font-semibold mb-10">About Me! <span className="text-accent">₊*✦</span></span>
         <div
             className="overflow-y-hidden flex flex-col tablet:flex-row w-screen items-center tablet:px-32 laptop:px-52 desktop:px-72 justify-center px-10 ">    
-             <Image className="mb-5" src="/profile.png" alt="profile" width={200} height={200}/>
             <p className="text-base md:text-[18px] font-sans font-light ml-10">   
                 My name is Yuni Park, and I recently graduated from <HighlightedText>
                 Khoury College of Computer Science at Northeastern University </HighlightedText> with a degree in <HighlightedText>Computer Science and Cognitive Psychology</HighlightedText>.
