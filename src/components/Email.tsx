c14e12e12783b128c84e0fd036e12025f2ec2deb
park.yun <park.yun@northeastern.edu>
2025-11-01 20:47:32 -0400

    replace deco

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -3,7 +3,7 @@ import SideBars from "@/ui/SideBars"
 const Email = () => {
     return (
     <SideBars left={false}>  
-        <a className="text-xs align-base rotate-90 text-center"></a>  
+        <a className="text-xs align-base rotate-90 text-center">🫧𓇼𓏲*ੈ✩‧₊˚</a>  
     </SideBars>)
 }
 
