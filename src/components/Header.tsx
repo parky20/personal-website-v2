f3f483a3d67fb8e59afab38d7c25bd957695f6c8
park.yun <park.yun@northeastern.edu>
2025-11-01 20:45:37 -0400

    no index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
    <header className="z-50 fixed top-0 flex items-center tablet:left-0 tablet:px-10 tablet:py-8
                         justify-between w-screen h-20 font-semibold bg-white bg-opacity-60 dark:bg-dark-background"> 
         <Logo />  
-        {/* <NavMenu /> */}
+        <NavMenu />
         <div className="hidden tablet:flex">
             <ToggleTheme />
         </div> 
